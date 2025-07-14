a6c773d86607588e66315a6bdefb4e6235f9ccfc
Housam Ziad <hi@hmz.ie>
2025-07-14 12:04:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
 const allSearchSites = [
   {
     name: "Google Maps",
-    url: "https://www.google.com/maps/search/%QUERY%"
+    url: "https://www.google.com/maps/search/%QUERY%",
+    id: "google_maps"
   },
   {
     name: "Wikipedia",
-    url: "https://en.wikipedia.org/w/index.php?search=%QUERY%&title=Special%3ASearch&go=Go"
+    url: "https://en.wikipedia.org/w/index.php?search=%QUERY%&title=Special%3ASearch&go=Go",
+    id: "wikipedia"
   },
   {
     name: "Google",
-    url: "https://www.google.com/search?q=%QUERY%"
+    url: "https://www.google.com/search?q=%QUERY%",
+    id: "google"
   }
 ];
 
 allSearchSites.forEach(function (site) {
   chrome.contextMenus.create({
+    id: site.id,
     title: site.name,
-    contexts: ["selection"],
-    onclick: function (word) {
-      var query = word.selectionText;
-      chrome.tabs.create({
-        url: site.url.replace("%QUERY%", query)
-      });
-    }
+    contexts: ["selection"]
   });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  const site = allSearchSites.find(s => s.id === info.menuItemId);
+  if (site && info.selectionText) {
+    const query = encodeURIComponent(info.selectionText);
+    chrome.tabs.create({
+      url: site.url.replace("%QUERY%", query)
+    });
+  }
 });
